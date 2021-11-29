9c3759256be83d5d4eec95206afae0715130d80d
Daniel Burnette <dcburnette999@gmail.com>
2021-11-29 13:26:17 -0600

    cleanup unused variables

--- src/ErrorMessage.tsx
+++ src/ErrorMessage.tsx
@@ -1,11 +1,3 @@
-import React from "react";
-
-// errorMessage() {
-//   const { errorMessage } = this.state;
-//   const shouldDisplay = this.state.errorMessage.length > 0;
-//   return shouldDisplay ? <p role="alert">{errorMessage}</p> : null;
-// }
-
 function ErrorMessage(props: { message: string }) {
   const { message } = props;
   return <p role="alert">{message}</p>;
